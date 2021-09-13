638a078cf90d8299677ac88cefbe08b05899ef34
Rounak Singh <rounaksingh@Rounaks-MacBook-Air.local>
2021-09-13 19:37:52 +0530

    New Updates 13/09/21

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume-web';
+  topRes = 10;
+  leftRes = 10;
+  isDivVisible = false;
+  isExpVisible = false;
+
+  mouseLeft(event) {
+    this.topRes = event.clientX;
+    this.leftRes = event.clientY;
+  }
+
+  menuVisible() {
+    if(this.isDivVisible)
+      this.isDivVisible = false;
+    else
+      this.isDivVisible = true;
+  }
+
+  expVisible() {
+    if(this.isExpVisible)
+      this.isExpVisible = false;
+    else
+      this.isExpVisible = true;
+  }
+
 }
